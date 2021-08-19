d0e9d2f8a9fe0fece89a2be7d2b98837e48227ab
udbbbn <udbbbn@163.com>
2021-08-19 17:10:51 +0800

    feat: function-key 补全判断

--- function-keys.ts
+++ function-keys.ts
@@ -12,8 +12,13 @@ type FunctionBaseModule = {
   funcProps3: Function;
 };
 
+// 非严格模式下 undefined 跟 null 是所有类型的子类 且两者可互相兼容
+// (undefined extends null || null extends undefined) === true
+// 所以此处仅用一个判断
+type FilterInvalid<T> = T extends undefined ? never : T;
+
 type FunctionKeys<T extends object> = {
-  [K in keyof T]: T[K] extends Function ? K : never;
+  [K in keyof T]: FilterInvalid<T[K]> extends Function ? K : never;
 }[keyof T];
 
 type FunctionResult = FunctionKeys<FunctionBaseModule>;

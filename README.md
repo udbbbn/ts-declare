15cd90b751cb24195b4e18f3845c3b68213db760
udbbbn <udbbbn@163.com>
2021-08-19 18:11:59 +0800

    feat: 增强pick 补充写法

--- README.md
+++ README.md
@@ -5,3 +5,4 @@
 - symmetric-difference --- 获取没有同时存在于 T 和 U 内的类型
 - function-keys --- 获取 T 中所有类型为函数的 key 组成的联合类型
 - optional-keys --- 获取 T 中可选类型
+- enhance-pick --- 增强pick 可指定获取的类型
--- enhance-pick.ts
+++ enhance-pick.ts
@@ -10,10 +10,37 @@ type EnhancePick<T, I> = Pick<
 >;
 
 type EnhancePickRes = EnhancePick<
-  { key1: number; key2: string; key3: number },
+  { key1: number; key2: string | number; key3: number },
   number
 >;
 // type EnhancePickRes = {
 //     key1: number;
+//     key3: number;
+// }
+
+// 考虑欠缺了 上面的方法没办法判断 联合类型 例如
+// { key2: number | null | undefined } 按一般写法来判断 key2 应该被提取出来
+// (非严格模式下)
+// 因为可以传入 number 类型
+// 因此需要再写一个方法
+// 原理是 使用 元组 避免 extends 分发
+
+type EnhancePickExact<T, I> = Pick<
+  T,
+  {
+    [K in keyof T]: [T[K]] extends [I]
+      ? [I] extends [T[K]]
+        ? K
+        : never
+      : never;
+  }[keyof T]
+>;
+type EnhancePickRes2 = EnhancePickExact<
+  { key1: number; key2: number | null | undefined; key3: number },
+  number
+>;
+// type EnhancePickRes2 = {
+//     key1: number;
+//     key2: number | null | undefined;
 //     key3: number;
 // }
